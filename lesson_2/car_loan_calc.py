bf44bdcf080124f4f792e3b8b1c952770caa4867
jbielawski <foxandthefiddle@comcast.net>
2024-07-23 12:35:53 -0500

    Make PEP 8 changes from Pylint suggestions

--- lesson_2/car_loan_calc.py
+++ lesson_2/car_loan_calc.py
@@ -1,27 +1,27 @@
 # You'll need three pieces of information:
 
 #   the loan amount
-#   the Annual Percentage Rate (APR)
+#   the Annual Percentage Rate (apr)
 #   the loan duration
 
 # From the above, you'll need to calculate the following two things:
 
-#   monthly interest rate (APR / 12)
+#   monthly interest rate (apr / 12)
 #   loan duration in months
 
 def prompt(message):
     print(f'==> {message}')
 
 def invalid_input(input):
-    try: 
+    try:
         float(input)
     except ValueError:
         return True
-    else:
-        return False
-    
-def monthly_rate(APR):
-    return float((float(APR) / 100) / 12)
+
+    return False
+
+def monthly_rate(percentage_rate):
+    return float((float(percentage_rate) / 100) / 12)
 
 def years_to_months(months):
     return float(months) * 12
@@ -36,13 +36,13 @@ while invalid_input(loan_amount):
     loan_amount = input()
 
 prompt("Thank you! Please enter the Annual Percentage rate.")
-APR = input()
+apr = input()
 
-while invalid_input(APR):
+while invalid_input(apr):
     prompt('Please enter a decimal value')
-    APR = input()
+    apr = input()
 
-monthly_interest_rate = monthly_rate(APR)
+monthly_interest_rate = monthly_rate(apr)
 prompt("Thank you! Please enter the duration of the loan in years.")
 loan_duration = input()
 
@@ -52,6 +52,7 @@ while invalid_input(loan_duration):
 monthly_duration = years_to_months(loan_duration)
 prompt("Thanks!")
 
-monthly_payment = float(loan_amount) * (float(monthly_interest_rate) / (1 - (1 + float(monthly_interest_rate)) ** (-float(monthly_duration))))
+monthly_payment = float(loan_amount) * (float(monthly_interest_rate) / (
+    (1 - (1 + float(monthly_interest_rate)) ** (-float(monthly_duration)))))
 
 prompt(f"your monthly payment is ${'%.2f' % monthly_payment}")
